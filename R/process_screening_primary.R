80764b3c4caea6bf296194e66e4bf85ad805def7
Ernest Guevarra <ernest@guevarra.io>
2026-05-30 15:54:09 +0100

    complete claude llm workflow

--- R/process_screening_primary.R
+++ R/process_screening_primary.R
@@ -1,5 +1,5 @@
 #'
-#' Process Gemini primary screening of title and abstract
+#' Process primary screening of title and abstract
 #' 
 #' @param search_df A `data.frame()` object of the search output.
 #' @param screen_results A `data.frame()` of search screening results.
@@ -8,13 +8,13 @@
 #'   new variables for calculated screening results.
 #' 
 #' @examples
-#' gemini_process_screening_primary(search_full_processed, gemini_screen_primary)
+#' process_screening_primary(search_full_processed, gemini_screen_primary)
 #' 
 #' @export
 #' 
 
-gemini_process_screening_primary <- function(search_df,
-                                             screen_results) {
+process_screening_primary <- function(search_df,
+                                      screen_results) {
   tibble::tibble(
     search_df, 
     screen_results |>
