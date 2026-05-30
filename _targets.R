e842b9d8a51ea1a6a014c7d80fee928d9ba375fb
Ernest Guevarra <ernest@guevarra.io>
2026-05-30 22:24:52 +0100

    edit .gitattributes

--- _targets.R
+++ _targets.R
@@ -175,6 +175,26 @@ gemma_ollama_targets <- tar_plan(
 )
 
 
+## Ollama Deepseek R1 LLM targets ----
+deepseek_ollama_targets <- tar_plan(
+  deepseek_model = "deepseek-r1:671b",
+  tar_target(
+    name = deepseek_reviewer,
+    command = ellmer::chat_ollama(
+      system_prompt = screening_context_prompt, model = deepseek_model
+    )
+  ),
+  tar_target(
+    name = deepseek_test_screen_primary,
+    command = deepseek_screen_articles(
+      deepseek_reviewer = deepseek_reviewer,
+      query = screening_prompt
+    ),
+    pattern = map(screening_prompt)
+  )
+)
+
+
 ## Analysis targets ----
 analysis_targets <- tar_plan(
   
